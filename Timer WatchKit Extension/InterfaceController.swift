51ea07356ef101f2d9d014b5f02fe91aac574cd5
Emily Horsman <me@emilyhorsman.com>
2018-05-15 21:14:15 -0700

    Add a toggling timer!!!!!11

--- Timer WatchKit Extension/InterfaceController.swift
+++ Timer WatchKit Extension/InterfaceController.swift
@@ -11,21 +11,37 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    @IBOutlet var timer: WKInterfaceTimer!
+
+    @IBOutlet var toggle: WKInterfaceButton!
+
+    var hasStarted: Bool = false
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
     }
-    
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-    
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+
+    @IBAction func handleToggle() {
+        if (hasStarted) {
+            self.timer.stop()
+            self.toggle.setTitle("Start")
+        } else {
+            self.timer.setDate(Date())
+            self.timer.start()
+            self.toggle.setTitle("Stop")
+        }
+
+        hasStarted = !hasStarted
     }
 
 }
